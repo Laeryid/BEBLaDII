ecfb962c7f6dc4e60d6a64bbca818d154ab5378f
Bogdan Buliakov <67539637+Laeryid@users.noreply.github.com>
2026-05-07 21:27:05 +0200

    ошибка теперь равномерно распределена mse = cos

--- experiments/train_phase1_TPU_fsdp.py
+++ experiments/train_phase1_TPU_fsdp.py
@@ -147,7 +147,7 @@ def train():
         clip_threshold=1.0
     )
     scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=500, T_mult=1, eta_min=2e-5)
-    criterion = DistillationLoss(cos_weight=20.0)
+    criterion = DistillationLoss()
 
     global_step = 0
     start_epoch = 0
